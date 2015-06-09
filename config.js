e43b5126bf9c9d9320375fd089507930f30693e9
いっちまん <s.111chikiii@gmail.com>
2015-06-09 18:42:29 +0900

    sample

---
+++ config.js
@@ -0,0 +1,8 @@
+// window.GoogleSamples = window.GoogleSamples || {};
+// window.GoogleSamples.Config = window.GoogleSamples.Config || {
+//   gcmAPIKey: '<Your Public API Key from https://console.developers.google.com>'
+// };
+window.GoogleSamples = window.GoogleSamples || {};
+window.GoogleSamples.Config = window.GoogleSamples.Config || {
+  gcmAPIKey: 'AIzaSyAW2E9vJsnaeyZWGgEVOQPDCJxre4k7kYo'
+};
